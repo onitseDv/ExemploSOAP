d4a04937bddf9435af366064bb4b74a82c97c88d
onitseDv <onitsedv@hotmail.com>
2022-03-18 19:22:35 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1{
+    class Program {
+        static async Task Main(string[] args) {
+            ServiceReference1.SOAPDemoSoapClient client = new ServiceReference1.SOAPDemoSoapClient();
+            var response = await client.AddIntegerAsync(1, 5);
+            Console.WriteLine($"{response.ToString()}");
+
+
+            var response2 = await client.DivideIntegerAsync(10, 2);
+            Console.WriteLine($"{response2.ToString()}");
+
+
+            
+            var response3 = await client.FindPersonAsync("20");
+            Console.WriteLine($"{response3.Name}");
+            
+        }
+
+    }
+}
+
